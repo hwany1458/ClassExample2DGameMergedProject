e0c149644af54b7df979630c0b112b2b07d86abe
hwany1458 <hwany1458@empal.com>
2023-11-14 13:29:07 +0900

    commit

--- Assets/Scripts/DataManagerWithJsonUtility.cs
+++ Assets/Scripts/DataManagerWithJsonUtility.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 using System.IO;
 
 
-public class TestPlayerController : MonoBehaviour
+public class DataManagerWithJsonUtility : MonoBehaviour
 {
     // 필요한 실질적인 데이터 (객체로부터 제공받음)
-    public TestPlayerData playerData;
+    public PlayerData playerData;
 
     // Start is called before the first frame update
-    void Start() {}
+    void Start() 
+    {
+        AddData();
+    }
 
     // Update is called once per frame
     void Update() {}
@@ -24,31 +27,44 @@ public class TestPlayerController : MonoBehaviour
     // 여기서는 ContextMenu를 사용
     // 컴포넌트의 뒤에 함수를 실행하는 메뉴버튼을 추가
     [ContextMenu("To Json Data")]
-    public void SavePlayerDataToJson()
+    public void WriteToJson()
     {
         // 유니티에 내장된 JsonUtility 사용
         // Json 문자열로 변형하고 싶은 오브젝트를 넘겨줌 (Json 형태로 포맷팅된 문자열이 나옴)        
         // 결과를 문자열로 받음
         string jsonData = JsonUtility.ToJson(playerData, true);
+
         // Json 데이터를 파일로 저장
         // 현재 실행중인 유니티 프로젝트 경로에 저장 : Application.dataPath 
-        string path = Path.Combine(Application.dataPath, "playerData.json");
-        File.WriteAllText(path, jsonData);
+        string savingFileName = Path.Combine(Application.dataPath, "playerData.json");
+        File.WriteAllText(savingFileName, jsonData);
     }
 
     [ContextMenu("From Json Data")]
-    public void LoadPlayerDataFromJson()
+    public void ReadFromJson()
     {
         // 현재 실행중인 유니티 프로젝트 경로에 저장 : Application.dataPath 
-        string path = Path.Combine(Application.dataPath, "playerData.json");
+        string readFileName = Path.Combine(Application.dataPath, "playerData.json");
+
         // JSON 데이터를 가져와서, JSON 데이터로부터 오브젝트를 생성해서 데이터를 써줌
         // 해당경로로부터 문자열 데이터를 읽어서 문자열로 반환
-        string jsonData = File.ReadAllText(path);
+        string jsonData = File.ReadAllText(readFileName);
         // JsonUtility의 FromJson을 통해 JSON으로부터 오브젝트로 Deserialize
         // 오브젝트로 복구시킬 대상을 제네릭으로 명시해야 함
         // (JSON 데이터를 PlayerData 오브젝트로 복구시킴)
         // 출력으로 나온 PlayerData 타입의 오브젝트를 PlayerController가 가졌던 PlayerData로 덮어쓰기
-        playerData = JsonUtility.FromJson<TestPlayerData>(jsonData);
+        playerData = JsonUtility.FromJson<PlayerData>(jsonData);
+    }
+
+    void AddData()
+    {
+        playerData.name = "YongHwan";
+        playerData.affiliation = "Wonkwang University";
+        playerData.gender = "Man";
+        playerData.age = 50;
+        playerData.lectures.Add("C Langages");
+        playerData.lectures.Add("Understanding of Game Engine");
+        playerData.lectures.Add("Capstone Design");
     }
 }
 
@@ -65,12 +81,13 @@ public class TestPlayerController : MonoBehaviour
 // Player 정보만 저장할 단순 컨테이너 클래스를 생성
 // Serialize 되어 있지 않기 때문에 인스팩터에 나타나지 않음 
 [System.Serializable]
-public class TestPlayerData
+public class PlayerData
 {
     // Player에 필요한 데이터를 명시
     public string name;
     public string affiliation;
     public string gender;
     public int age;
-    public string[] lectures;
+    //public string[] lectures;
+    public List<string> lectures;
 }
