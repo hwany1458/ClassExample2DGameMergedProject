ad968e1775d2c155b1f9abd83dcb9272d81f3b24
hwany1458 <hwany1458@empal.com>
2023-11-24 18:22:59 +0900

    20231124

---
+++ Assets/Scripts/TestDatabaseDataManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDatabaseDataManager : MonoBehaviour
+{
+    public InputField inputName;
+    public InputField inputCount;
+    public InputField inputScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetString("Name", inputName.text);
+        PlayerPrefs.SetInt("Count", int.Parse(inputCount.text));
+        PlayerPrefs.SetInt("Score", int.Parse(inputScore.text));
+    }
+
+    public void Load()
+    {
+        if(PlayerPrefs.HasKey("Name"))
+        {
+            string getName = PlayerPrefs.GetString("Name");
+            int getCount = PlayerPrefs.GetInt("Count");
+            int getScore = PlayerPrefs.GetInt("Score");
+
+            print(getName + " " + getCount + " " + getScore);
+        }
+    }
+}
